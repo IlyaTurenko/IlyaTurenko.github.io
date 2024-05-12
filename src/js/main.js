30b26471b6122ee789c2612dc22fee1cf5fa5a36
IlyaTurenko <131530750+IlyaTurenko@users.noreply.github.com>
2024-05-12 14:10:50 +0300

    first

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded" ,function(){
+    document.getElementById("burger").addEventListener("click",function(){
+        document.querySelector("ec_nav_panel_upper").classList.toggle("open")
+    })
+})
